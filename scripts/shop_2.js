0b008135cd793e26af202de397e03221cea18755
Andreaardilado <andreaardila@gmail.com>
2024-02-07 12:07:20 -0500

    importar

--- scripts/shop_2.js
+++ scripts/shop_2.js
@@ -1,3 +1,5 @@
+import { productosShopPage } from "../modules/products.js";
+
 /*const rutaShop_2Page = [
     {
         inicio: "Home",
@@ -23,27 +25,42 @@ return pre_compra;
 }
 llenarRuta();*/
 
-const productoPreCompra = {
-    imagenes: {
-       uno: "../images/Image 5.jpg",
-       dos: "../images/Image 5.png",
-       tres:"../images/Image 5 (1).png",
-       cuatro: "../images/Image 5 (2).png"
-    }
-    
-  };
+const idProduct = JSON.parse(localStorage.getItem("productId"));
 
-const fotoDetalle = document.getElementById("detalles-izquierdo");
-console.log(fotoDetalle);
+const productoId = () => productosShopPage.find((producto) => producto.id == idProduct);
+const productoPreCompra = productoId();
+
+
+
+const listaFotos = document.getElementById("detalles-izquierdo");
 
 const recorrerFotoDetalle = () => {
-    fotoDetalle.innerHTML = productoPreCompra.map((imagen) => {
-        `<a href="${imagen.imagenes}"> 
-        <img class="izquierdo-detalles-fotos-column" src="${imagen.imagenes}" alt="">
-        </a>`
-    }).join("");
-    return fotoDetalle;
-}
+  listaFotos.innerHTML = productoPreCompra.imagenes
+    .map((imagen) => {
+      return `<a href=""> 
+      <img class="izquierdo-detalles-fotos-column" src="${imagen}" alt="">
+    </a>`;
+    })
+    .join("");
+};
+
 recorrerFotoDetalle();
 
+const fotosDetalles = document.getElementById("detalles-izquierdo-1");
+fotosDetalles.innerHTML = `<img class="izquierdo-detalles-fotos-row" src="${productoPreCompra.imagen}" alt="">`;
 
+const nombreProducto = document.getElementById("detallesJoyas");
+nombreProducto.innerHTML = `<h2 class="titulo">${productoPreCompra.nombre}</h2>
+  <p class="codigo">${productoPreCompra.codigo}</p>
+                        <p class="precio">${productoPreCompra.precioUnitario}</p>
+                        <a class="seleccion-tonos" href="">Color-Rose Gold</a>`;
+
+const listaColor = document.getElementById("coloresJoya");
+ listaColor.innerHTML = productoPreCompra.colores.map((color)=>{
+  return `<img class="tonos" src="${color.imagen}" alt=""></img>`
+ }).join("");
+
+const listaTalla = document.getElementById("tallasBotones");
+listaColor.innerHTML = productoPreCompra.cantidadTalla.map ((tallas)=>{
+  return `<button class="talla">${tallas.talla}</button>`
+}).join("");
--- scripts/shop_page.js
+++ scripts/shop_page.js
@@ -1,210 +1,4 @@
-const productosShopPage = [
-  {
-    id: 12,
-    nombre: "Luxury Gems Necklace",
-    codigo: "N0123",
-    precioUnitario: 168.76,
-    tipoAccesorio: "Necklace",
-    imagenes: "../images/Luxury Gems Necklace.png",
-    descripcion: "Collar gema rosada",
-    cantidadColor: {
-      "Rose Gold": 0,
-      Silver: 0,
-      Gold: 0,
-    },
-    cantidadTalla: {
-      48: 0,
-      50: 0,
-      52: 0,
-      54: 0,
-      56: 0,
-      58: 0,
-      60: 0,
-      62: 0,
-      64: 0,
-      66: 0,
-      68: 0,
-      70: 0,
-    },
-  },
-  {
-    id: 1,
-    nombre: "Aurora Ring",
-    codigo: "AN123",
-    precioUnitario: 125.28,
-    tipoAccesorio: "anillo",
-    imagenes: "../images/Aurora Ring.png",
-    descripcion: "Anillo diamantado olas",
-    cantidadColor: {
-      "Rose Gold": 31,
-      Silver: 38,
-    },
-    cantidadTalla: {
-      48: 3,
-      50: 7,
-      52: 0,
-      54: 6,
-      56: 2,
-      58: 9,
-      60: 10,
-      62: 4,
-      64: 15,
-      66: 5,
-      68: 2,
-      70: 6,
-    },
-  },
-  {
-    id: 13,
-    nombre: "Reflections Necklace",
-    codigo: "N0124",
-    precioUnitario: 620.73,
-    tipoAccesorio: "Necklace",
-    imagenes: "../images/Reflections Necklace.png",
-    descripcion: "Collar serpiente rojo-negro-dorado",
-    cantidadColor: {
-      "Rose Gold": 0,
-      Silver: 0,
-      Gold: 0,
-    },
-  },
-  {
-    id: 2,
-    nombre: "Dreamy Infinity Ring",
-    codigo: "AN124",
-    precioUnitario: 327.71,
-    tipoAccesorio: "anillo",
-    imagenes: "../images/Dreamy Infinity Ring.png",
-    descripcion: "Anillo diamante rosa",
-    cantidadColor: {
-      "Rose Gold": 40,
-      Silver: 18,
-    },
-    cantidadTalla: {
-      48: 4,
-      50: 7,
-      52: 0,
-      54: 0,
-      56: 5,
-      58: 10,
-      60: 10,
-      62: 4,
-      64: 5,
-      66: 5,
-      68: 2,
-      70: 6,
-    },
-  },
-  {
-    id: 3,
-    nombre: "Opulent Jewels Ring",
-    codigo: "AN125",
-    precioUnitario: 168.76,
-    tipoAccesorio: "anillo",
-    imagenes: "../images/Opulent Jewels Ring.png",
-    descripcion: "Anillo gato",
-    cantidadColor: {
-      "Rose Gold": 31,
-      Silver: 38,
-    },
-    cantidadTalla: {
-      48: 3,
-      50: 7,
-      52: 0,
-      54: 6,
-      56: 2,
-      58: 9,
-      60: 10,
-      62: 4,
-      64: 15,
-      66: 5,
-      68: 2,
-      70: 6,
-    },
-  },
-  {
-    id: 29,
-    nombre: "Serene Solitaire Earrings",
-    codigo: "AR124",
-    precioUnitario: 620.73,
-    tipoAccesorio: "aretes",
-    imagenes: "../images/Timeless Halo Earrings.png",
-    descripcion: "Aretes crital cuadrado",
-    cantidadColor: {
-      Gold: 2,
-      Silver: 18,
-    },
-    cantidadTalla: {
-      48: 2,
-      50: 0,
-      52: 2,
-      54: 2,
-      56: 2,
-      58: 2,
-      60: 2,
-      62: 0,
-      64: 2,
-      66: 2,
-      68: 2,
-      70: 2,
-    },
-  },
-  
-  {
-    id: 20,
-    nombre: "Exquisite Earrings",
-    codigo: "AR125",
-    precioUnitario: 620.73,
-    tipoAccesorio: "aretes",
-    imagenes: "../images/Exquisite Earrings.png",
-    descripcion: "Aretes gota de agua tallados",
-    cantidadColor: {
-      Gold: 9,
-      Silver: 11,
-    },
-    cantidadTalla: {
-      48: 2,
-      50: 0,
-      52: 2,
-      54: 2,
-      56: 2,
-      58: 2,
-      60: 2,
-      62: 0,
-      64: 2,
-      66: 2,
-      68: 2,
-      70: 2,
-    },
-  },
-  {
-    id: 4,
-    nombre: "Timeless Elegance Ring",
-    codigo: "AN126",
-    precioUnitario: 125.28,
-    tipoAccesorio: "anillo",
-    imagenes: "../images/Timeless Elegance Ring.png",
-    descripcion: "3 en 1 lila",
-    cantidadColor: {
-      "Rose Gold": 40,
-      Silver: 18,
-    },
-    cantidadTalla: {
-      48: 4,
-      50: 7,
-      52: 0,
-      54: 0,
-      56: 5,
-      58: 10,
-      60: 10,
-      62: 4,
-      64: 5,
-      66: 5,
-      68: 2,
-      70: 6,
-    },
-  },
-];
+import { productosShopPage } from "../modules/products.js";
 
 // const contenedorProductos = document.getElementById("productos-shop-page");
 const listaProductos = document.getElementById("lista-productos");
@@ -215,9 +9,9 @@ console.log(listaProductos);//capturamos la lista de productos del html
 const poblarLista = () => {
   listaProductos.innerHTML  = productosShopPage.map((producto) => {
    return/*Hacemos retornar un string y cada item que queremos que se pinte lo asignamos ()por eso producto. + el elemento que queremos tomar de la lista*/`
-    <a class="link_catalogo" href="../pages/shop_2.html">
+    <a id=${producto.id} class="link_catalogo" href="#">
       <figure class="catalogo">
-        <img class="fotos-catalogo" src="${producto.imagenes}" alt="Collar gema">
+        <img class="fotos-catalogo" src="${producto.imagen}" alt="Collar gema">
         <figcaption id="12" class="referencia-producto">${producto.nombre}</figcaption>
         <figcaption class="precio">$${producto.precioUnitario}</figcaption>
       </figure>
@@ -227,5 +21,17 @@ const poblarLista = () => {
 }
 poblarLista();
 
+const irADetalleProducto = () => {
+  const cards = document.querySelectorAll(".link_catalogo");
+  
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      const productId = card.getAttribute("id");
+      localStorage.setItem("productId", JSON.stringify(productId));
+      location.href = "../pages/shop_2.html";
+    })
+  })
+}
 
+irADetalleProducto();
 
